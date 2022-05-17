9691f667e9915d783c64760cbba8a9e552625b02
Kenny Clint <Corey_taylor_slipknot@hotmail.fr>
2022-05-17 15:38:39 +0200

    Ajout avec fonction anti-CORS, routes user

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes.user");
+const app =express();
+
+mongoose.connect(,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(function(req, res, next) 
+{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
