ef42ae95f6f84295af8d48d901b8e7b04c83e7b9
Kenny Clint <Corey_taylor_slipknot@hotmail.fr>
2022-05-17 12:30:43 +0200

    Intégration de la route '/api/auth'

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes.user");
 const app =express();
 
 mongoose.connect("mongodb+srv://Ken:ChillCountry@cluster0.vh7rs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
@@ -8,12 +9,14 @@ mongoose.connect("mongodb+srv://Ken:ChillCountry@cluster0.vh7rs.mongodb.net/myFi
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res, next) => {
+app.use(function(req, res, next) 
+{
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
