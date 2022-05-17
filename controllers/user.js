f412b11003e754b953768225bad0310be0ec9380
Kenny Clint <Corey_taylor_slipknot@hotmail.fr>
2022-05-17 12:31:45 +0200

    Ajout de la fonction 'signup'

---
+++ controllers/user.js
@@ -0,0 +1,29 @@
+const bcrypt = require("bcrypt");
+
+const User = require("../models/User");
+
+exports.signup = function(req, res, next) 
+{
+	bcrypt.hash(req.body.password, 10)
+	.then(function (hash)
+	{
+		const user = new User(
+		{
+			email : req.body.email,
+			password : hash
+		});
+		user.save()
+		.then(function()
+		{
+			res.status(201).json({message : "Utilisateur créé"})
+		}
+		.catch(function(error)
+		{
+			res.status(400).json({error});
+		};
+	}
+		.catch(function(error)
+		{
+			res.status(500).json({error});
+		});
+	};
