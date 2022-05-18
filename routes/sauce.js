a8c3c5927710f8323e3867416615da875a003286
Kenny Clint <Corey_taylor_slipknot@hotmail.fr>
2022-05-18 16:59:57 +0200

    sauce.js : Ajout de la route 'likeSauce'

--- routes/sauce.js
+++ routes/sauce.js
@@ -8,5 +8,6 @@ router.post("/", sauceCtrl.createSauce);
 router.get("/:id", sauceCtrl.getOneSauce);
 router.put("/:id", sauceCtrl.modifySauce);
 router.delete("/:id", sauceCtrl.deleteSauce);
+router.post("/:id/like", sauceCtrl.likeSauce);
 
 module.exports = router;
