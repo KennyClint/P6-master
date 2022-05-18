0d13700ae03b525d0724f5a4a73b7d83f0babcab
Kenny Clint <Corey_taylor_slipknot@hotmail.fr>
2022-05-18 17:07:18 +0200

    sauce.js : Ajout du middleware authentification aux routes

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+const auth = require(../middleware/auth);
+
 const sauceCtrl = require("../controllers/sauce");
 
-router.get("/", sauceCtrl.getAllSauce);
-router.post("/", sauceCtrl.createSauce);
-router.get("/:id", sauceCtrl.getOneSauce);
-router.put("/:id", sauceCtrl.modifySauce);
-router.delete("/:id", sauceCtrl.deleteSauce);
-router.post("/:id/like", sauceCtrl.likeSauce);
+router.get("/", auth,sauceCtrl.getAllSauce);
+router.post("/", auth, sauceCtrl.createSauce);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.put("/:id", auth, sauceCtrl.modifySauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
+router.post("/:id/like", auth, sauceCtrl.likeSauce);
 
 module.exports = router;
