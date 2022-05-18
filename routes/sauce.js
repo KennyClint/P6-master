7fe63482aef11f21f7f2b32f7a1847f244dd764d
Kenny Clint <Corey_taylor_slipknot@hotmail.fr>
2022-05-18 16:39:43 +0200

    sauce.js : Ajout de la route 'deleteSauce'

--- routes/sauce.js
+++ routes/sauce.js
@@ -7,5 +7,6 @@ router.get("/", sauceCtrl.getAllSauce);
 router.post("/", sauceCtrl.createSauce);
 router.get("/:id", sauceCtrl.getOneSauce);
 router.put("/:id", sauceCtrl.modifySauce);
+router.delete("/:id", sauceCtrl.deleteSauce);
 
 module.exports = router;
