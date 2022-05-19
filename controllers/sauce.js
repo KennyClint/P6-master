69e5cccd6a125f6f7b08858d411a5489acdd8ab2
Kenny Lemée <mephisdante@gmail.com>
2022-05-19 12:04:44 +0200

    sauce.js : Ajout d'initialisation des 'likes' et 'dislikes' à 0 dans la fonction 'createSauce'

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -18,6 +18,8 @@ exports.createSauce = function(req, res, next)
 	delete sauceObject._id;
 	const sauce = new Sauce ({
 		...sauceObject,
+		likes : 0,
+		dislikes : 0,
 		imageUrl : `${req.protocol}://${req.get("host")}/images/${req.file.filename}`	
 	});
 	sauce.save()
